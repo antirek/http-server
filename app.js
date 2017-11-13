8f1e573bd7778bdd3b1a56dc5483b27ca6f5932a
Sergey Dmitriev <serge.dmitriev@gmail.com>
2017-11-13 14:17:33 +0700

    fix

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ var bodyParser = require('body-parser')
 
 let app = express()
 app.use(bodyParser.json())
+app.set('views', './views')
+app.set('view engine', 'pug')
+
+app.get('/test', (req, res) => {
+
+    res.render('index', {title: 'test', message: 'test'})
+})
 
 app.all('/', (req, res) => {
 
@@ -12,5 +19,6 @@ app.all('/', (req, res) => {
     res.status(200).send('OK')
 
 })
+
 
 app.listen(config.port, () => {console.log('app started')})

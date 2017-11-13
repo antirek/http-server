bdda9aff0e07ffb891bab9f3b35d646155873076
Sergey Dmitriev <serge.dmitriev@gmail.com>
2017-11-13 16:02:07 +0700

    fix

--- app.js
+++ app.js
@@ -9,16 +9,12 @@ app.set('views', './views')
 app.set('view engine', 'pug')
 
 app.get('/test', (req, res) => {
-
     res.render('index', {title: 'test', message: 'test'})
 })
 
 app.all('/', (req, res) => {
-
     console.log(req.body)
     res.status(200).send('OK')
-
 })
-
 
 app.listen(config.port, () => {console.log('app started')})
